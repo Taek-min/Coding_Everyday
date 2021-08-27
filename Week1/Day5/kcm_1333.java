813763df346482fbb128bceb62af9f95c216542c
ckdals6932 <ckdals6932@naver.com>
2021-08-27 15:12:53 +0900

    day5_changmin

---
+++ Week1/Day5/kcm_1333.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.util.Scanner;
+public class kcm_1333 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int l = in.nextInt();
+		int d = in.nextInt(); 
+		int count= 1; // 곡 수 
+		int time = 0; // 현재 시간
+		
+		while(true) {
+			time = time + l;
+			for(int i =time; i < time + 5; i++) {
+				if(i % d == 0) {
+					System.out.println(i);
+					return;
+				}
+				
+			}
+			count++; // 곡 수.
+			if(count > n) {
+				for(int j =1; ; j++) {
+					if(d * j >= time) {
+						System.out.println(d*j);
+						return;
+					}
+				}
+			}
+			time += 5;
+		}
+	}		
+}
+
+
+
