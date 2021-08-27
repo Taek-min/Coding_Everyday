5d872c3ae28e2f7b0073f0320871e37df8ce718e
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2021-08-27 15:56:24 +0900

    5일차

---
+++ Week1/Day5/shm_1333.java
@@ -0,0 +1,29 @@
+package Day5;
+		
+import java.util.Scanner;		
+								   
+public class shm_1333 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int L = sc.nextInt();
+		int D = sc.nextInt();
+		int cmp = 0;
+		
+		for (int i = 0; i < N; i++) { 
+			for (int t = (i*(L+5)+L); t < (i+1)*(L+5); t++) {
+				if(t % D == 0) {	// 간주와 알람 일치 시 
+					cmp = t;
+					break;
+				}
+			}
+			if(cmp != 0)
+				break;
+		}
+		if (cmp == 0) //앨범이 끝나고도 못들음
+			System.out.println(N * (L + 5) - 5 + D - (N * (L + 5) - 5) % D);
+		else
+			System.out.println(cmp);
+		}
+}
