734dfce8a452b0b729ddc3de36708d367afcec9b
김진홍 <deepred.kim@gmail.com>
2021-08-28 11:55:08 +0900

    5일차 피드백

--- Week1/Day5/jtm_1333.cpp
+++ Week1/Day5/jtm_1333.cpp
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
 #define MAX_LENGTH 100000
+//// char이 아닌 boolean 배열로 하고 isPlayingThisSecond 등으로 이름을 지어줬으면 더 좋았을 듯
+//// check_time: '시간을 확인한다'로는 아무런 유의미한 정보도 얻을 수 없었음
 char check_time[MAX_LENGTH];
 
 int N, L, D;
 
 int main()
 {
+    //// 문제 입력이 하나가 아니라 여러개면 N, L, D처럼 문제에서 지어준 이름 그대로 쓰기보다는
+    //// songAmount 식으로 지어주는게 훨씬 가독성이 나음
     scanf("%d", &N);
     scanf("%d", &L);
     scanf("%d", &D);
     int cnt = 0;
 
     ////생각 치 못한 방법이라 신선ㄴ해요!
+    //// check_time이 1일때는 노래가 재생중이라 못 듣는중, 0일때는 들을 수 있는 상태로 쓰고
+    //// 벨이 울리는 단위로 배열을 순회하면서 0일 경우를 만나면 그걸 반환하게 했네
+    //// 메모리 효율면에서 조금 아쉬운 점이 있지만 되게 창의력 있는 로직인듯 굳
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < L; j++)
@@ -29,6 +36,7 @@ int main()
     while (check_time[sec])
         sec += D;
 
+    //// 29~30줄 때문에 이 while문은 실행될 일이 없음. 33~34줄을 빼도 '맞았습니다' 잘 나옴
     while (check_time[sec])
         sec--;
 
--- Week1/Day5/shm_1333.java
+++ Week1/Day5/shm_1333.java
@@ -1,18 +1,24 @@
 package Day5;
 		
 import java.util.Scanner;		
-								   
+
 public class shm_1333 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//// 문제 입력이 하나가 아니라 여러개면 N, L, D처럼 문제에서 지어준 이름 그대로 쓰기보다는
+    //// songAmount 식으로 지어주는게 훨씬 가독성이 나음
 		int N = sc.nextInt();
 		int L = sc.nextInt();
 		int D = sc.nextInt();
+		//// cmp라는 이름으론 변수의 의도를 알기 어려움
 		int cmp = 0;
 		//// 공식 활용을 잘 한거 같아요
 		for (int i = 0; i < N; i++) { 
+			//// (i*(L+5)+L) 같은 복잡한 수식에 canHearStart, canHearEnd처럼 이름을 붙여주면 가독성이 훨씬 좋아짐!
 			for (int t = (i*(L+5)+L); t < (i+1)*(L+5); t++) {
+				//// 변수 작명, 함수 작명등으로 코드의 의도를 최대한 설명해보고 정말 불가피할때 다는게 주석임 (이외에는 안 다는게 좋음)
+				//// 주석 대신에 let isHeardTheRingtone = t % d == 0; 식으로 변수 이름으로 나타내주면 더 좋을듯
 				if(t % D == 0) {	// 간주와 알람 일치 시 
 					cmp = t;
 					break;
@@ -21,6 +27,8 @@ public class shm_1333 {
 			if(cmp != 0)
 				break;
 		}
+
+		//// 이것도 변수 이름을 heardTimeBeforeSongEnd 등으로 충분히 표현 가능
 		if (cmp == 0) //앨범이 끝나고도 못들음
 			System.out.println(N * (L + 5) - 5 + D - (N * (L + 5) - 5) % D);
 		else
