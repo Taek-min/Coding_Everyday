6f146c92d84bae41818c199f7a5bae4fef0cc6a9
Taek-min <jtm11245@gmail.com>
2021-08-30 00:05:30 +0900

    update

---
+++ Week1/Day7/jtm_2875.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int female male, intern;
+		female = sc.nextInt();
+		male = sc.nextInt();
+		intern = sc.nextInt();
+		
+		int  team= 0;
+		while(female >= 2 && male >=1) {
+			female -= 2;
+			male--;
+			team ++;
+		}
+		
+		while(female + male < intern) {
+			female += 2;
+			male++;
+			team--;
+		}
+		System.out.println(team);
+	}
+}
+
