88d78fe04f4cf34554df629a6c722a372140006e
Taek-min <jtm11245@gmail.com>
2021-09-02 12:38:14 +0900

    Readme 수정

--- Week2/Day10/lye_16283.py
+++ Week2/Day10/lye_16283.py
@@ -5,6 +5,7 @@ goatCnt = 0
 
 for i in range(1, totalCnt):
 	## 식이 조금 복잡한 편이라서 따로 변수로 빼줘서 이름으로 설명해줬다면 가독성이 더 좋았을 것 같아요!
+	## if문 조건식에서 제가 놓친부분을 깨달이갑니다.
 	if sheepGram * i + goatGram * (totalCnt - i) == totalGram:
 		if findNum:
 			findNum = False
