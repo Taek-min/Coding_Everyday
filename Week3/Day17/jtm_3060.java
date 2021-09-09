16326f7647273fd1bba1e4449ec087d7d867fd6b
Deepred <deepred.kim@gmail.com>
2021-09-09 12:07:54 +0900

    17일차 피드백

--- Week3/Day17/jtm_3060.java
+++ Week3/Day17/jtm_3060.java
@@ -4,8 +4,11 @@ class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		//// needPrey나 requirePrey나 똑같은 뜻으로 읽힙니다.
+		//// 두 변수의 용도 차이가 이름에서 명확히 드러나면 더 좋을 것 같습니다
 		int []needPrey = new int[6];
 		
+		//// 탭을 왜 두번씩 하신거죠
 		for(int j = 0; j < N; j++){
 		    	int day = 1;
 		    	int requirePrey = 0;
--- Week3/Day17/kcm_3060.java
+++ Week3/Day17/kcm_3060.java
@@ -10,6 +10,8 @@ public class kcm_3060 {
 		int testAmount = in.nextInt();
 		for(int i = 0; i < testAmount; i++) {
 			int feed = in.nextInt();
+			//// reFeed(먹이 다시), allFeed(먹이 전부) 변수명으로 뭘 말하고 싶은지 잘 모르겠어요.
+			//// 다른 분들 변수명 작명 참고해서 좀 더 명확하게 바꿔보시면 좋을 것 같습니다
 			int reFeed = feed;
 			int allFeed = 0;
 			int days = 1;
@@ -17,6 +19,7 @@ public class kcm_3060 {
 				pigs[j] = in.nextInt();
 				allFeed += pigs[j];
 			}
+			//// day 변수는 for문 밖에서 사용하지 못하니까 나중에 출력하기 위한 용도로 days 변수를 만든거 같은데, 고냥 for문 밖에서 day를 선언하면 for문 밖에서 사용할 수 있습니다
 			for(int day = 1; feed >= 0; day++) {
 				feed = reFeed;
 				if(day == 1) {
