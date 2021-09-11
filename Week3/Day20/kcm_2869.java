641f4d2367c7be9e63fae95f54708483b2f9cf65
ckdals6932 <ckdals6932@naver.com>
2021-09-11 13:36:13 +0900

    20일 문제

---
+++ Week3/Day20/kcm_2869.java
@@ -0,0 +1,20 @@
+package Day20;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class kcm_2869 {
+
+	public static void main(String[] args) throws IOException { 
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		double climbing = Integer.parseInt(st.nextToken());
+		double sliding =  Integer.parseInt(st.nextToken());
+		double goal =  Integer.parseInt(st.nextToken());
+		double day = (goal- climbing) / (climbing - sliding);
+		day = (int)Math.ceil(day);
+		System.out.println(day +1);
+	}
+}
+
