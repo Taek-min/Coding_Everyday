137a4a224d2f01fae9bbbb5abfbb164300985a00
Taek-min <jtm11245@gmail.com>
2021-09-14 01:00:59 +0900

    no message

---
+++ Week4/Day22/jtm_1924.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		int day = sc.nextInt();
+		
+		int[] dayOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		String[] dayOfWeeks = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+		
+		int totalDays = day;
+		for (int i = 0; i < month - 1; ++i) {
+			totalDays += dayOfMonth[i];
+		}
+		
+		System.out.println(dayOfWeeks[totalDays % 7]);
+	}
+}
