1e20a1671542b2ad4ffa5f6ad7cabb9e3167ca9e
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-09-13 12:40:08 +0900

    Update shm_2869.java

--- Week3/Day20/shm_2869.java
+++ Week3/Day20/shm_2869.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-//// 고생했어
+//// 고생했어 :22
 public class shm_2869 { //// 복사본이네
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
