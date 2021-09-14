52eb9545b2e62d0f098c837553a1689a698e4d7a
Taek-min <jtm11245@gmail.com>
2021-09-15 02:50:55 +0900

    update

---
+++ Week4/Day23/jtm_4619.java
@@ -0,0 +1,39 @@
+package algorithm;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int B = sc.nextInt();
+			int N = sc.nextInt();
+			
+			if(B == 0 && N == 0) {
+				break;
+			}
+			
+			int result = 1;
+			int preMultiplier = 1;
+			
+			for(int multiplier = 1; ; multiplier++) {
+				int tmp = 1;
+				
+				for(int i = 0; i < N; i++) {
+					tmp = tmp * multiplier;
+				}
+				
+				if(Math.abs(tmp - B) < Math.abs(B - preMultiplier)) {
+					result = multiplier;
+					preMultiplier = tmp;
+				}
+				
+				if(tmp > B) {
+					break;
+				}
+			}
+			System.out.println(result);
+		}
+	}
+}
