1a3bb7fe04643b52628a2324c1ac2f1af555d5d6
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-09-15 09:24:23 +0900

    Day23

---
+++ Week4/Day23/lye_4619.py
@@ -0,0 +1,12 @@
+import math
+	
+while True:
+	B, N = map(int, input().split(' '))
+	if B + N == 0:
+		break
+	A = int(math.pow(B, 1/N))
+	
+	if B - math.pow(A, N) < math.pow(A+1, N) - B:
+		print(A)
+	else:
+		print(A+1)
