e7223f1427e180cbcb018d22ba7b0c4ce4eaca9f
Taek-min <jtm11245@gmail.com>
2021-09-20 15:05:13 +0900

    update & feedback

--- Week4/Day28/shm_2440.java
+++ Week4/Day28/shm_2440.java
@@ -1,7 +1,7 @@
 package Day28;
 
 import java.util.Scanner;
-	//// 이문제 넷다 똑같이 풀었네,,, 깔꼼~~ :2
+	//// 이문제 넷다 똑같이 풀었네,,, 깔꼼~~ :2 : 33
 public class shm_2440 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
