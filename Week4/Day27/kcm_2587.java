887ecd3d84b0409a38f96d53f8c90b08fc6ecbf8
ckdals6932 <79955006+ckdals6932@users.noreply.github.com>
2021-09-20 15:02:40 +0900

    29일차 문제 및 28일자 피드백 및 27일차 피드백 반영

--- Week4/Day27/kcm_2587.java
+++ Week4/Day27/kcm_2587.java
@@ -1,4 +1,5 @@
 package Day27; //// 백준에 제출해서 맞은 코드로 커밋해주십쇼
+////                 ㄴ 이거 맞는데 ?
 
 import java.util.Scanner;
 import java.util.Arrays;
---
+++ Week5/Day29/kcm_9506.java
@@ -0,0 +1,38 @@
+package Day29;
+
+import java.util.Scanner;
+import java.util.*;
+
+
+public class kcm_9506 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while(true) {
+			int num = in.nextInt();
+			List<String> perfectNum = new ArrayList<String>();
+			int sum = 0;
+			String answer = num +" = ";
+			if(num == -1) {
+				break;
+			}
+			for(int i = 1; i<num; i++) {
+				if(num % i == 0) {
+					perfectNum.add(Integer.toString(i)); 
+					sum += i;
+				}
+			}
+			if(num == sum) {
+				for(int j = 0; j <(perfectNum.size()- 1); j++) {
+					answer += perfectNum.get(j);
+					answer += " + ";
+				}
+				answer += perfectNum.get(perfectNum.size()-1);
+				System.out.println(answer);
+			}	
+			else {
+				System.out.println(num + " is NOT perfect.");
+			}
+		}
+	}
+}
