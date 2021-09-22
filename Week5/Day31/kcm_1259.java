39925e6470c84b4131a893da08fc7b7413e71b99
ckdals6932 <ckdals6932@naver.com>
2021-09-22 16:05:59 +0900

    31일차 문제 수정

--- Week5/Day31/kcm_1259.java
+++ Week5/Day31/kcm_1259.java
@@ -1,9 +1,7 @@
-package Day31;
-
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class kcm_1259 {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
