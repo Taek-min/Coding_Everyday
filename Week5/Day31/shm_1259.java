e53d9197b3a62d888c48130828c4f48edc79286e
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2021-09-23 16:07:30 +0900

    Update shm_1259.java

--- Week5/Day31/shm_1259.java
+++ Week5/Day31/shm_1259.java
@@ -4,7 +4,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		while(true) {
 			String palindrome = sc.nextLine();
-			//// accord는 무슨뜻이에요 찾아보니까 합의라고만 나오는데 : 일치하다란 뜻도 있을걸
+			//// accord는 무슨뜻이에요 찾아보니까 합의라고만 나오는데 : 일치하다란 뜻도 있을걸 : 네.. 일치하다.. 그래서 일치한 횟수..
 			int accordCnt = 0;
 			if(palindrome.equals("0")) {
 				break;
