acd6d0e7fd360bf9aa01546c752dc3ded1ae12e5
Taek-min <jtm11245@gmail.com>
2021-09-23 15:50:48 +0900

    문제풀이 & FeedBack

---
+++ Week5/Day30/jtm_fb_10769.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		int check = 0;
+		int smile = 0;
+		int sad = 0;
+		
+		//// char인데 이름은 웨 string
+		for(char inputChar : str.toCharArray()) {	//// 향상된 for문 Good 코드 재밌네 : 22 우와ㅏ 이걸 이렇게,,,?? 신기하넿ㅎ :3
+			if (inputChar == ':' || (inputChar == '-') && check == 1) {
+				check++;
+				continue;
+			}
+			else if(check == 2) {
+				if(inputChar == '(') {
+					sad++;
+				}
+				else if(inputChar ==')') {
+					smile++;
+				}
+			}
+			check = 0;
+		}
+		
+		int difference = smile - sad;
+		if(difference == 0) {
+			if(smile == 0) System.out.println("none");
+			else System.out.println("unsure");
+		}
+		else System.out.println(difference > 0 ? "happy" : "sad");
+	}
+}
---
+++ Week5/Day32/jtm_8595.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int wordLength = sc.nextInt();
+		char[] word = sc.next().toCharArray();
+		long sumHidden = 0, tmp = 0;
+		
+		for (int i = 0; i < wordLength; i++) {
+			if (word[i] >= '0' && word[i] <= '9') {
+				tmp = tmp * 10 + word[i] - '0';
+			} 
+			else {
+				sumHidden += tmp; //temporary
+	                tmp = 0;
+			}
+		}
+		sumHidden += tmp;
+		System.out.println(sumHidden);
+	}
+}
