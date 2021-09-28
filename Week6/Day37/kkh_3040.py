7004e6d70c03e5a6f29ca684e67da79d0033be34
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-09-28 22:36:18 +0900

    Create kkh_3040.py

---
+++ Week6/Day37/kkh_3040.py
@@ -0,0 +1,12 @@
+capNum = []
+for i in range(0, 9):
+	capNum.append(int(input()))
+
+for i in range(0, len(capNum)):
+	for j in range(i+1, len(capNum)):
+		if(sum(capNum) - capNum[i] - capNum[j] == 100):
+			del capNum[i]
+			del capNum[j-1]
+			for k in capNum:
+				print(k)
+			break
