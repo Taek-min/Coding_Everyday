aff08a9e3c6a7b6fad7ffa1670ddf954203f24af
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-09-29 00:01:51 +0900

    ..

---
+++ Week6/Day38/kkh_17202.py
@@ -0,0 +1,20 @@
+phoneA = [int(a) for a in str(input())]
+phoneB = [int(a) for a in str(input())]
+fullNum = []
+possibility = []
+
+for i in range(0, len(list(phoneA))): #번호 하나로 합치기
+    fullNum.append(phoneA[i])
+    fullNum.append(phoneB[i])
+
+while (len(fullNum) > 2) : #앞 + 뒤 계산
+	for i in range(0, len(fullNum)-1):
+		num = fullNum[i] + fullNum[i+1]
+		if num >= 10:
+			num -= 10
+		possibility.append(num)
+	fullNum = possibility
+	possibility = []
+   
+for i in range (0, len(fullNum)): # 출력
+    print(fullNum[i], end = '')
