1566cd96677b62fe9745751e45c5e0d003330fec
Taek-min <jtm11245@gmail.com>
2021-09-29 00:11:49 +0900

    update kkh_17202.py

--- Week6/Day38/kkh_17202.py
+++ Week6/Day38/kkh_17202.py
@@ -3,11 +3,11 @@ phoneB = [int(a) for a in str(input())]
 fullNum = []
 possibility = []
 
-for i in range(0, len(list(phoneA))): #번호 하나로 합치기
+for i in range(0, len(list(phoneA))): 
     fullNum.append(phoneA[i])
     fullNum.append(phoneB[i])
 
-while (len(fullNum) > 2) : #앞 + 뒤 계산
+while (len(fullNum) > 2) : 
 	for i in range(0, len(fullNum)-1):
 		num = fullNum[i] + fullNum[i+1]
 		if num >= 10:
