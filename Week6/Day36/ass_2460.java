718ea909003854e5e699eccd4664b1d3a1c31eb5
sungsuahn <saop1739@naver.com>
2021-09-29 15:05:03 +0900

    Update ass_2460.java

--- Week6/Day36/ass_2460.java
+++ Week6/Day36/ass_2460.java
@@ -1,22 +1,18 @@
 import java.util.Scanner; //// import 후 줄바꿈 하나는 국룰: 2
+
 public class Main//// 성수 역시 잘푸네 ^^ 
 {
    public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
       int min=0;
       int max=0;
-      //// 변수 이름은 의미있게 지어주십쇼
-      int hum=0;
       for(int i = 0 ; i<10;i++)
       {
           int subout = scan.nextInt();
           int subin = scan.nextInt();
-          //// min+=subin - subout 으로 하면 hum 변수선언 없이 풀 수 있을것 같아요. :2 : 3 :4 :5
-          min=subin-subout;
-          hum=hum+min;
-          
-          if(hum>=max){
-              max=hum;
+          min=subin-subout; 
+          if(min>=max){
+              max=min;
           }
       }
       System.out.println(max);
