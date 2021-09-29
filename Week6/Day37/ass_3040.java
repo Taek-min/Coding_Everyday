4d61c77ec4088b24d5b71dc9f7cfa9af0c4618ab
Taek-min <jtm11245@gmail.com>
2021-09-29 15:38:35 +0900

    feedback

--- Week6/Day37/ass_3040.java
+++ Week6/Day37/ass_3040.java
@@ -7,12 +7,16 @@ public class Main {
         {
             num[f] = scan.nextInt();
         }
+        //// avr, ck 가 어떤 변수명을 의미하는지 모르겠습니다. 
         int avr = 0;
+        //// ck 라는 변수를 굳이 쓸 필요가 없어보입니다.
         int ck = 100;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
+                //// 조건에서 중복이 없다고 해서 굳이 필요없는 코드입니다.
                 if (i == j)
                     continue;
+                    //// for문 괄호도 없고 avr += num[g]; 들여쓰기도 안되어있습니다.
                 for (int g = 0; g < 9; g++)
                 avr += num[g];
                 avr = avr - num[i] - num[j];
