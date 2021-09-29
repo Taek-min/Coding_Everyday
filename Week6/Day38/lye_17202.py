afd68cff218b3f084a9e3c6e05f3405ea0d62124
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-09-29 16:55:45 +0900

    Day38_lye

---
+++ Week6/Day38/lye_17202.py
@@ -0,0 +1,20 @@
+def get_num_arr():
+	str = input()
+	numArr = []
+	
+	for i in range(0, len(str)):
+		numArr.append(int(str[i]))
+	return numArr
+
+firstArr = get_num_arr()
+secondArr = get_num_arr()
+
+for i in range(0, len(firstArr)):
+	firstArr.insert(2*i+1, secondArr[i])
+
+while(len(firstArr) > 2):
+	for i in range(0, len(firstArr) - 1):
+		firstArr[i] = (firstArr[i] + firstArr[i+1]) % 10
+	firstArr.pop()
+	
+print("%d%d"%(firstArr[0], firstArr[1]))
