61cb394248dddabdc0357f44a656a4b4f6a97638
sungsuahn <91382828+sungsuahn@users.noreply.github.com>
2021-09-30 15:04:28 +0900

    Update kcm_17202.java

--- Week6/Day38/kcm_17202.java
+++ Week6/Day38/kcm_17202.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 import java.util.Arrays; 
 
 public class Main {////스마트 창민 
-//// 여기 줄바꿈 없애는게 국룰
+//// 여기 줄바꿈 없애는게 국룰 :2
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int[] phoneNum1 = Arrays.stream(in.next().split("")).mapToInt(Integer::parseInt).toArray();
+		int[] phoneNum1 = Arrays.stream(in.next().split("")).mapToInt(Integer::parseInt).toArray(); //이렇게 배열로 넣을수 있군요! 하나 배워갑니다.
 		int[] phoneNum2 = Arrays.stream(in.next().split("")).mapToInt(Integer::parseInt).toArray();
 		int firseIndex = 0;
 		int	secondIndex = 0;
@@ -25,8 +25,8 @@ public class Main {////스마트 창민
 				compatibility[j] = (compatibility[j] + compatibility[j+1]) % 10;
 				System.out.println("k : " + k + " j : " + j + " num[j] : " + compatibility[j]);
 			}
-		} //// 줄바꿈이 하나도 없어서 답답해유 이런 코드 맥락 바뀌는 곳에서 한번씩 해주면 읽기 편할거같아요
+		} //// 줄바꿈이 하나도 없어서 답답해유 이런 코드 맥락 바뀌는 곳에서 한번씩 해주면 읽기 편할거같아요 :2
 		System.out.print(compatibility[0]);
 		System.out.print(compatibility[1]);	
 	}
-}
+}//좀 치네요
