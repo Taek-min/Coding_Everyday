d6f7f8fabc9ce828686b8cceea20dbd2f8e87f6d
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-09-30 16:58:41 +0900

    Create kkh_2566.py

---
+++ Week6/Day39/kkh_2566.py
@@ -0,0 +1,13 @@
+maxNum = 0
+maxLine = 0
+maxRow = 0
+for i in range(0, 9):
+	num = list(map(int, input().split(" ")))
+	
+	if maxNum < max(num):
+		maxNum = max(num)
+		maxLine = i + 1
+		maxRow = num.index(max(num)) + 1
+
+print(maxNum)
+print(maxLine, maxRow)
