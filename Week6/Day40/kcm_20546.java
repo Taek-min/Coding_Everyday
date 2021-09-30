253daba745fe8fc9ca4fb0fe8e8a065dbd761fed
ckdals6932 <79955006+ckdals6932@users.noreply.github.com>
2021-10-01 02:00:12 +0900

    40일차 문제 수정

--- Week6/Day40/kcm_20546.java
+++ Week6/Day40/kcm_20546.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
 		int junhyunCoin = 0;
 		int sungminCoin = 0;
-		int junhyun = in.nextInt();
-		int sungmin = junhyun;
+		int junhyunMoney = in.nextInt();
+		int sungminMoney = junhyunMoney;
 		int downcount = 0;
 		int upcount = 0;
 		int todayStock[] = new int[14];
-		int temp = 0;
-
+		int beforeValue = 0;
+		
 		for(int i = 0; i < todayStock.length; i++) {
 			todayStock[i] = in.nextInt();
-			temp = todayStock[0];
+			beforeValue = todayStock[0];
 		}
 		for(int j = 0; j < todayStock.length-1; j++) {
-			if(junhyun / todayStock[j] > 0) {
-				junhyunCoin += junhyun / todayStock[j];
-				junhyun -= ((junhyun / todayStock[j]) * todayStock[j]);	
+			if(junhyunMoney / todayStock[j] > 0) {
+				junhyunCoin += junhyunMoney / todayStock[j];
+				junhyunMoney -= ((junhyunMoney / todayStock[j]) * todayStock[j]);	
 			}
-
-			if(temp < todayStock[j]) {
+			if(beforeValue < todayStock[j]) {
 				upcount++;
 				downcount = 0;
 			}
-			else if(temp > todayStock[j]){
+			else if(beforeValue > todayStock[j]){
 				downcount++;
 				upcount = 0;
 			}
@@ -34,28 +34,27 @@ public class Main {
 				upcount = 0;
 				downcount = 0;
 			}
-			
 			if(upcount == 3) {
 				if(sungminCoin > 0) {
-					sungmin += sungminCoin * todayStock[j+1];
+					sungminMoney += sungminCoin * todayStock[j+1];
 					sungminCoin = 0;
 				}
 				upcount = 0;
 			}
 			else if(downcount == 3) {
-				if(sungmin / todayStock[j] > 0) {
-					sungminCoin += sungmin / todayStock[j];
-					sungmin -= ((sungmin / todayStock[j]) * todayStock[j]);
+				if(sungminMoney / todayStock[j] > 0) {
+					sungminCoin += sungminMoney / todayStock[j];
+					sungminMoney -= ((sungminMoney / todayStock[j]) * todayStock[j]);
 				}
 				downcount = 0;
 			}
-			temp = todayStock[j];
+			beforeValue = todayStock[j];
 		}
-
-		if(sungmin + sungminCoin * todayStock[13] == junhyun + junhyunCoin * todayStock[13]) {
+		
+		if(sungminMoney + sungminCoin * todayStock[13] == junhyunMoney + junhyunCoin * todayStock[13]) {
 			System.out.println("SAMESAME");
 		}
-		else if(sungmin + sungminCoin * todayStock[13] > junhyun + junhyunCoin * todayStock[13]) {
+		else if(sungminMoney + sungminCoin * todayStock[13] > junhyunMoney + junhyunCoin * todayStock[13]) {
 			System.out.println("TIMING");
 		}
 		else {
