acfb508fb03606089a2e0f116b2cc7d7cc27834a
sungsuahn <saop1739@naver.com>
2021-09-30 15:39:06 +0900

    Create ass_2566.java

---
+++ Week6/Day39/ass_2566.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main
+{
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int[][] Num = new int [9][9];
+      
+      int max=0;
+	  int row=0;
+	  int column=0;
+	  
+      for(int j=0;j<9;j++){
+	    for(int i=0;i<9;i++){
+	      Num[j][i]+=scan.nextInt();
+	      
+          if(Num[j][i]>max){
+	        max=Num[j][i];
+	        row=j+1;
+	        column=i+1;
+          }
+
+	    }    
+	  }
+	  
+      System.out.println(max);
+	  System.out.printf("%d %d",row,column);
+      
+      }
+}
