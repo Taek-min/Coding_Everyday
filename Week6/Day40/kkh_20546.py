ce5d44d266eb6c5e159e03193007d059c4b05f0a
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-01 15:04:16 +0900

    Update kkh_20546.py

--- Week6/Day40/kkh_20546.py
+++ Week6/Day40/kkh_20546.py
@@ -1,9 +1,5 @@
-junhyun = int(input())
-sungmin = junhyun
-holdingJunhyun = 0
-holdingSungmin = 0
-upCnt = 0
-downCnt = 0
+junhyun = sungmin = int(input())
+holdingJunhyun = holdingSungmin = upCnt = downCnt = 0
 machineDuck = list(map(int, input().split(" ")))
 
 for i in range(1, len(machineDuck)):
@@ -24,15 +20,14 @@ for i in range(1, len(machineDuck)):
 	if upCnt == 3: 
 		sungmin += holdingSungmin * machineDuck[i]
 		holdingSungmin = 0
-		upCnt = 0
 	elif downCnt >= 3: 
 		if sungmin >= machineDuck[i]: 
 			holdingSungmin += sungmin // machineDuck[i]
 			sungmin -= machineDuck[i] * (sungmin // machineDuck[i])
 
-if(junhyun + machineDuck[len(machineDuck)-1] * holdingJunhyun > sungmin + machineDuck[len(machineDuck)-1] * holdingSungmin)	:
+if junhyun + machineDuck[len(machineDuck)-1] * holdingJunhyun > sungmin + machineDuck[len(machineDuck)-1] * holdingSungmin	:
 	print("BNP")
-elif(junhyun + machineDuck[len(machineDuck)-1] * holdingJunhyun < sungmin + machineDuck[len(machineDuck)-1] * holdingSungmin) :
+elif junhyun + machineDuck[len(machineDuck)-1] * holdingJunhyun < sungmin + machineDuck[len(machineDuck)-1] * holdingSungmin :
 	print("TIMING")
 else:
 	print("SAMESAME")
