52aa4d2a9f4d6e0ad61fb3d8322c068e0d0caaba
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-01 18:33:37 +0900

    Update kkh_1453.py

--- Week6/Day41/kkh_1453.py
+++ Week6/Day41/kkh_1453.py
@@ -1,8 +1,8 @@
 peopelNum = int(input())
 choiceList = list(map(int, input().split()))
+choiceList.sort()
 reject = 0
 
-choiceList.sort()
 for i in range(0, len(choiceList)-1):
 	if choiceList[i] == choiceList[i+1]:
 		reject += 1
