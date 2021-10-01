9d393c9799704b7f44351340eab9c37a83fcd50d
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-01 18:30:32 +0900

    .

---
+++ Week6/Day38/kkh_fb_17202.py
@@ -0,0 +1,20 @@
+phoneA = [int(a) for a in str(input())] ## 파이썬 활용 개잘하시는데? 리스트 내포 ㄷㄷ :22
+phoneB = [int(a) for a in str(input())]
+fullNum = []
+possibility = []
+
+for i in range(0, len(list(phoneA))): 
+    fullNum.append(phoneA[i])
+    fullNum.append(phoneB[i])
+
+while (len(fullNum) > 2) : 
+	for i in range(0, len(fullNum)-1):
+		num = (fullNum[i] + fullNum[i+1]) % 10
+		#if num >= 10: ## 10에 대한 나머지 구하면 간단함!
+			#num -= 10
+		possibility.append(num)
+	fullNum = possibility
+	possibility = []
+   
+print(*fullNum, sep='')
+## 잘해 아주~ :2 :3 :4 : s5 :55
