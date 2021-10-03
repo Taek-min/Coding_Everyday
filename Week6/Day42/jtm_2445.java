13f12bcf832b1093f49f7e1711a428aadb0cf9ee
Taek-min <jtm11245@gmail.com>
2021-10-03 12:34:06 +0900

    42일차 문제풀이 및 피드백 반영

---
+++ Week6/Day42/jtm_2445.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int num = sc.nextInt();
+    	
+    	for(int i = 1; i <= num; i++) {
+    		for (int j = i; 0 < j; j--) System.out.print("*");
+    		for (int k = 2 * num - 2 * i; k > 0;k--) System.out.print(" ");
+    		for (int l = i; 0 < l; l--)System.out.print("*");
+    		System.out.println();
+    	}
+    	
+    	for(int i = num - 1; 0 < i; i--) {
+    		for (int j = i; 0 < j; j--) System.out.print("*");
+    		for (int k = 2 * num - 2 * i; 0 < k; k--) System.out.print(" ");
+    		for (int l = i; 0 < l; l--)System.out.print("*");
+    		System.out.println();
+    	}
+    }
+}
