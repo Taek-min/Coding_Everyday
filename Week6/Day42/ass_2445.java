205b7722783ce7575a6f6ac07239c24d33b947ef
김진홍 <deepred.kim@gmail.com>
2021-10-04 14:49:50 +0900

    42일차 피드백

--- Week6/Day42/ass_2445.java
+++ Week6/Day42/ass_2445.java
@@ -3,7 +3,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int starLine = scan.nextInt();
+		int starLine = scan.nextInt(); //// 입력이 한 개고 이름 짓기 애매하다면 그냥 문제에서 말하는대로 N이라 지어도 괜춘한듯
 		for(int i = 0 ;i<starLine;i++){
 		    for(int k = 0 ; k<i+1;k++){
 		        System.out.printf("*");
@@ -17,7 +17,7 @@ public class Main
 		    System.out.println("");
 		}
 		for(int i = starLine-1; i>0; i--){
-		       for(int k = 0 ; k<i;k++){
+		       for(int k = 0 ; k<i;k++){ //// 탭정리!
 		        System.out.printf("*");
 		    }
 		    for(int j = 0; j<(starLine-i)*2;j++){
@@ -26,7 +26,7 @@ public class Main
 		    for(int k = 0 ; k<i;k++){
 		        System.out.printf("*");
 		    }
-		    System.out.println("");
+		    System.out.println(""); //// "" 없어도 됨
 		}
 	}
 }
