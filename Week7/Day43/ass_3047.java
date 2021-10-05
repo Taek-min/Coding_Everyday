0cfedd004a691dd92d01f4f600f1f5b8d05042b8
sungsuahn <saop1739@naver.com>
2021-10-05 12:02:05 +0900

    day43

---
+++ Week7/Day43/ass_3047.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num[] = new int [3];
+		for(int i=0;i<3;i++){
+		    num[i] = scan.nextInt();
+		}
+		Arrays.sort(num);
+		scan.nextLine();
+		String Alphabet = scan.nextLine();
+		
+		for(int j = 0; j < 3; j++) {
+			if(Alphabet.charAt(j) == 'A') {
+				System.out.print(num[0]);
+			}
+			if(Alphabet.charAt(j) == 'B') {
+				System.out.print(num[1]);
+			}
+			if(Alphabet.charAt(j) == 'C') {
+				System.out.print(num[2]);
+			}
+			System.out.print(" ");
+		}	
+    }
+}
