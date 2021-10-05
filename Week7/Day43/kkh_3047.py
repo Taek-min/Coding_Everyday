ded44cecd026f8a7b4b0026e00e2936a8c0e7fa4
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-10-06 02:17:24 +0900

    Day43_lye

--- Week7/Day43/kkh_3047.py
+++ Week7/Day43/kkh_3047.py
@@ -1,5 +1,5 @@
 num = list(map(int, input().split()))
-char = [a for a in str(input())]	## 여기 배워갑니당 ^0^ :2 : 3
+char = [a for a in str(input())]	## 여기 배워갑니당 ^0^ :2 : 3:4
 num.sort()
 
 ## if 없이 num[ord(str.charAt(i)) - ord('A')] 출력해도 됨
---
+++ Week7/Day44/lye_1475.py
@@ -0,0 +1,13 @@
+import math
+roomNum = input()
+numCnt = []
+
+for i in range(0, 10):
+	numCnt.append(roomNum.count(str(i)))
+	
+numCnt[6] += numCnt[9]
+numCnt[6] = math.ceil(numCnt[6]/2)
+numCnt[9] = 0
+maxIndex = numCnt.index(max(numCnt))
+
+print(numCnt[maxIndex])
