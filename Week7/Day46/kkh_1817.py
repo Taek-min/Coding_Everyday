b31d8f3eb30472b4842d4c9f35d92ed4e5f65afb
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-08 01:00:16 +0900

    1817

---
+++ Week7/Day46/kkh_1817.py
@@ -0,0 +1,17 @@
+nm = list(map(int, input().split()))
+
+if nm[0] == 0:
+	print(0)
+else:
+	boxWeight = list(map(int, input().split()))
+	boxCnt = 1
+
+	weight = 0
+	for i in range(0, nm[0]): 
+		if(weight + boxWeight[i] <= nm[1]) :
+			weight += boxWeight[i]
+		else :
+			weight = 0
+			weight += boxWeight[i]
+			boxCnt += 1
+	print(boxCnt)
