304fd60be51b672931017b741742357b2a9bda7d
Deepred <deepred.kim@gmail.com>
2021-10-07 09:47:21 +0900

    Update kcm_14656.java

--- Week7/Day45/kcm_14656.java
+++ Week7/Day45/kcm_14656.java
@@ -6,7 +6,7 @@ public class kcm_14656 {
 		Scanner in = new Scanner(System.in);
 		int amount = in.nextInt();
 		int nums[] = new int[amount];
-		int count = 0; //// 이 아래같은데에 줄바꿈 넣어주새요 답답해요
+		int count = 0; //// 이 아래같은데에 줄바꿈 하나씩 넣으면 읽기 좋을 것 같아요
 		for(int i = 0; i < nums.length; i++) {
 			nums[i] = in.nextInt();
 			if(nums[i] != i+1) {
