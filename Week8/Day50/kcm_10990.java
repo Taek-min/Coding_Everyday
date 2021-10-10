0ac37aed56742354bbb7cc2acc8b899e9766fb5c
ckdals6932 <79955006+ckdals6932@users.noreply.github.com>
2021-10-10 21:26:09 +0900

    50일차 문제
    내일 알바 때문에 문제만 미리 올릴게요

---
+++ Week8/Day50/kcm_10990.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int line = in.nextInt();
+		int num1 = line;
+		int num2 = line;
+		Loop2:
+		for(int i = line; i > 0; i--) {
+			Loop1:
+			for(int j = 1; j < line * 2; j++) {
+				if(j == num1 || j == num2) {
+					System.out.printf("*");
+					if(j == num2) {
+						num1--;
+						num2++;
+						System.out.println("");
+						break Loop1;
+					}
+				}
+				else {
+					System.out.printf(" ");
+				}
+			}
+		}
+	}
+}
