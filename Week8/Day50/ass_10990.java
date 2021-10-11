951385f1d949352d6c7c99bfa827231d95d0c2dd
sungsuahn <saop1739@naver.com>
2021-10-12 00:21:34 +0900

    d50

---
+++ Week8/Day50/ass_10990.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int line = scan.nextInt();
+		for(int i = 0 ; i < line ; i++){
+			fo(int k = 0 ; k < i ; k++){
+				if(k==(line-1)){
+					System.out.printf("*");
+				}
+				for(int j=0;)
+			}
+		}
+	}
+}
