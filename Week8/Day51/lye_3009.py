98dd606baa1b198021a4715d82261b00009048d1
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-10-13 09:14:34 +0900

    Day51_lye

---
+++ Week8/Day51/lye_3009.py
@@ -0,0 +1,17 @@
+xPos = []
+yPos = []
+
+def find_different(numList):
+	if numList[0]==numList[1]:
+		return numList[2]
+	elif numList[1]==numList[2]:
+		return numList[0]
+	else:
+		return numList[1]
+		
+for i in range(0,3):
+	x,y = map(int, input().split())
+	xPos.append(x)
+	yPos.append(y)
+
+print(find_different(xPos), find_different(yPos))
