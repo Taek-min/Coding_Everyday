a11b79999709a7cea59a1c480a53451fd8db805a
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2021-10-13 19:12:44 +0900

    Day52: problem solving & Feedback

---
+++ Week8/Day52/shm_2506.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numOfProblem = sc.nextInt();
+		int score = 0;
+		int stack = 0;
+		
+		for(int i = 0; i < numOfProblem; i++) {
+			int problem = sc.nextInt();
+			if(problem == 1) {
+				if(stack > 0) {
+					stack++;
+					score += stack;
+				}
+				else{	
+					score++;
+					stack++;
+				}
+			}
+			else { 
+				stack = 0;
+			}
+		}
+		System.out.println(score);
+	}
+}
