3ff7178348ddf6c45bc5597b77f9210e8082b9a3
ckdals6932 <79955006+ckdals6932@users.noreply.github.com>
2021-10-13 10:00:07 +0900

    가히 문제 수정

--- Week8/Day51/kkh_3009.py
+++ Week8/Day51/kkh_3009.py
@@ -2,18 +2,15 @@ vertex = []
 x = []
 y = []
 for i in range(0, 3):
-	vertex = list(map(int, input().split()))
-	x.append(vertex[0])
-	y.append(vertex[1])
-	
-for i in x:
-	if x.count(i) == 2:
-		for j in range(0, 2):
-			x.remove(i)
-for i in y:
-	if y.count(i) == 2:
-		for j in range(0, 2):
-			y.remove(i)
-			
-print(x[0], y[0])
+   vertex = list(map(int, input().split()))
+   x.append(vertex[0])
+   y.append(vertex[1])
+   
+for i in range(0, 3):
+   if x.count(x[i]) == 1:
+      vertex[0] = x[i]
+   if y.count(y[i]) == 1:
+      vertex[1] = y[i]
+
+print(vertex[0], vertex[1])
 ## 굳
