b6f6bd59ffbbeea862468e30c9f279bf17be0d8c
Taek-min <jtm11245@gmail.com>
2021-10-16 01:08:04 +0900

    문제풀이

---
+++ Week8/Day54/jtm_1284.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int address[] = Arrays.stream(sc.next().split("")).mapToInt(Integer::parseInt).toArray();
+			int width = 0;
+			if(address[0] == 0) {
+				break;
+			}
+				
+			for(int i = 0; i < address.length; i++) {
+				if(address[i] == 1) {
+					width += 3;
+				}
+				else if(address[i] == 0) {
+					width += 5;
+				}
+				else {
+					width += 4;
+				}
+			}
+			System.out.println(width+1);
+		}
+	}
+}
