07852fb3ea7bac3a966cd36510914c07c8e64ae4
sungsuahn <saop1739@naver.com>
2021-10-15 17:39:30 +0900

    d

---
+++ Week8/Day54/ass_1284.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			String num = sc.nextLine();
+			int width = 2;
+			if (num.length() == 1 && num.charAt(0) == '0') {
+				break;
+			}
+			for (int i = 0 ; i < num.length() ; i++) {
+				if (num.charAt(i) == '1') { width += 2;}
+				else if (num.charAt(i) == '0') { width += 4;}
+				else { width += 3; }
+			}
+		width += num.length() - 1;
+		System.out.println(width);
+		}	
+	}
+}
