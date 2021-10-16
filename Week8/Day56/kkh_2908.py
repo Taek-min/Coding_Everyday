e5e391cf3c4e4382352890428de9069cae342662
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-16 17:00:09 +0900

    .

---
+++ Week8/Day56/kkh_2908.py
@@ -0,0 +1,9 @@
+num1, num2 = input().split()
+
+num1 = (int(num1) % 10 * 100) + (int(num1) % 100 // 10 * 10) + (int(num1) // 100)
+num2 = (int(num2) % 10 * 100) + (int(num2) % 100 // 10 * 10) + (int(num2) // 100)
+
+if num1 > num2:
+	print(num1)
+else:
+	print(num2)
