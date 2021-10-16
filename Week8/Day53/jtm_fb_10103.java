f5e83702c4c81d153fab6dfbda7898388e434e22
Taek-min <jtm11245@gmail.com>
2021-10-16 13:28:59 +0900

    문제풀이 및 피드백반영

---
+++ Week8/Day53/jtm_fb_10103.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {// 저와 생각이 같군요  :2 이번 문제 다 똑같이 풀엇네 : 아구잘해~ :이고 잘해~
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int round = sc.nextInt();
+		int changYoung = 100;
+		int sangDuck = 100;
+		for(int i = 0; i < round; i++) {
+			int changDice = sc.nextInt();
+			int sangDice = sc.nextInt();
+			
+			if(changDice < sangDice) {
+				changYoung -= sangDice;
+			}
+			else if(sangDice < changDice) {
+				sangDuck -= changDice;
+			}
+		}
+		System.out.printf("%d\n%d", changYoung, sangDuck); //이거 한번에 쓰는 방법 알아보고싶다.. // 진짜 몰라서 물어본거야 바꿔달라는거야 귀찮게 하네;;
+	}
+}
---
+++ Week8/Day55/jtm_14912.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int frequency = sc.nextInt();
+        int cnt = 0;
+ 
+        for (int i = 1; i <= n; i++) {
+            String nums = Integer.toString(i);
+ 
+            for (int j = 0; j < nums.length(); j++) {
+                int sequence = nums.charAt(j) - '0';
+ 
+                if (sequence == frequency) {
+                    cnt += 1;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
