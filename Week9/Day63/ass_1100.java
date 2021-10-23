973d11d2913db8d5595477cebfd8d5c954551e63
sungsuahn <saop1739@naver.com>
2021-10-24 03:55:17 +0900

    d63

---
+++ Week9/Day63/ass_1100.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);		
+		char chessBoard[][] = new char [8][8];
+		int cnt = 0;
+		
+		for(int i = 0; i < 8 ; i++){
+			String str = sc.nextLine();
+			for(int j = 0 ; j < 8 ; j++){
+				chessBoard[i][j] = str.charAt(j);
+				if((i+j)%2==0 && chessBoard[i][j] == 'F'){
+					cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+}
