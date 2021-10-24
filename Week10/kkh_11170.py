5f8e620f603570e79e23ab32712dc7243dce2106
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-24 17:42:08 +0900

    .

---
+++ Week10/kkh_11170.py
@@ -0,0 +1,11 @@
+T = int(input())
+cnt = 0
+
+for i in range(0, T):
+	nums = input().split()
+    
+	for j in range(int(nums[0]), int(nums[1]) + 1):
+		num = [str(a) for a in str(j)]
+		cnt += num.count('0')
+	print(cnt)
+	cnt = 0
