6692d22a5325ca0bff16abb374f0d01541957a61
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-26 01:21:09 +0900

    .

---
+++ Week10/Day65/kkh_4153.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		while(true) {
+			int line[] = new int[3];
+			
+			for(int i = 0; i < 3; i++) {
+				line[i] = in.nextInt();
+			}
+			
+			if(line[0] == 0 && line[1] == 0 && line[2] == 0) break;
+			
+			Arrays.sort(line);	
+			
+			if((line[0] * line[0]) + (line[1] * line[1]) == (line[2] * line[2])) {
+				System.out.println("right");
+			} else {
+				System.out.println("wrong");
+			}
+		}
+	}
+}
