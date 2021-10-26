ca1b95211038c018d7f07540870ef5580ecaaaf3
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-10-26 09:30:40 +0900

    day64_slove

---
+++ Week10/Day64/lye_11170.py
@@ -0,0 +1,8 @@
+caseCnt = int(input())
+
+for i in range(0, caseCnt):
+	start, end = map(int, input().split())
+	allNum = ""
+	for j in range(start, end + 1):
+		allNum += str(j)
+	print(allNum.count('0'))
