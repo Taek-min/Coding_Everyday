644376de5c7d8837f55653e23a8eb830eac6a182
Deepred <deepred.kim@gmail.com>
2021-10-28 10:18:18 +0900

    66일차 피드백

--- Week10/Day66/kcm_10874.java
+++ Week10/Day66/kcm_10874.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//// 깔끔
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -9,7 +10,7 @@ public class Main {
 		for(int i = 0; i < students; i++) {
 			String str = in.nextLine();
 			if(str.equals(answer)) {
-				System.out.println(i +1);
+				System.out.println(i +1); //// +1 사이 띄어쓰기!
 			}
 		}
 
