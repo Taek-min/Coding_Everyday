e1f07b2616e33345d317a2a8e522ddaafb206ced
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-29 00:50:32 +0900

    .

---
+++ Week10/Day66/kkh_10874.py
@@ -0,0 +1,10 @@
+N = int(input())
+for i in range(0, N):
+	answer = list(map(int, input().split()))
+
+	for j in range(0, len(answer)):
+		if (j % 5) + 1 == answer[j]:
+			if j == len(answer) - 1:
+				print(i + 1)
+		else:
+			break
