07e6a9ff8c9a1a3f24892933c1742bf810ebdced
Taek-min <jtm11245@gmail.com>
2021-10-29 13:48:21 +0900

    no message

---
+++ Week10/Day68/jtm_5598.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main
+{
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] charArray = br.readLine().toCharArray();
+		
+		for (int i = 0; i < charArray.length; ++i) {
+			charArray[i] = (char)(charArray[i] - 3);
+			if (charArray[i] < 65) {
+				charArray[i] += 26;
+			}
+			System.out.print(charArray[i]);
+		}   	
+	}
+}
+
+
