858ad72a7e318caa6d444726098fa2172b49ace9
ckdals6932 <ckdals6932@naver.com>
2021-10-29 13:41:06 +0900

    68일 문제 풀이

---
+++ Week10/Day68/kcm_5598.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String str[] = {"A", "B", "C", "D", "E", "F", "G" ,"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y", "Z"};
+		String caesarStr[] = {"D", "E","F", "G" ,"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y", "Z", "A", "B", "C"};
+		
+		String testStr[] = in.nextLine().split("");
+		for(int i = 0; i < testStr.length; i++) {
+			testStr[i] = str[Arrays.asList(caesarStr).indexOf(testStr[i])];
+			System.out.print(testStr[i]);
+		}
+	}
+
+}
