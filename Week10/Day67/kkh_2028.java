01c7a20b0b905d80bff46ae088a3431c7d088518
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-10-30 15:32:17 +0900

    .

---
+++ Week10/Day67/kkh_2028.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for (int i = 0; i < T; i++) {
+			int N = in.nextInt();
+			String squareN = Integer.toString(N * N);
+			
+			if(Integer.toString(N).equals(squareN.substring(squareN.length() - Integer.toString(N).length(), squareN.length()))) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+			
+		}
+	}
+}
