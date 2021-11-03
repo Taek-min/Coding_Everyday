9298981c1f426dac871c2269d428a15f8fae83d8
Taek-min <jtm11245@gmail.com>
2021-11-03 10:41:23 +0900

    no message

---
+++ Week11/Day72/jtm_2609.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int gcd = 0, lcm = 0;
+			int nums[] = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			Arrays.sort(nums);
+			for( int j = nums[0]; 0 < j; j--){
+				if(nums[0] % j == 0 && nums[1] % j == 0) {
+					gcd = j;
+					break;
+				}
+			}
+			lcm = nums[0] * nums[1] / gcd;
+			System.out.println(gcd);
+			System.out.println(lcm);
+	}
+}
+
+
---
+++ Week7/Day49/kkh_1292.py
@@ -0,0 +1,12 @@
+num = list(map(int, input().split()))
+sum = 0
+cnt = 0
+
+for i in range(0, num[1] + 1):
+	for j in range(1, i+1):
+		cnt += 1
+		if  cnt <= num[1] and cnt >= num[0]:
+			sum += i
+			
+print(sum)
+#### 역시 잘해 ~ :가히 잘하면서~~~ : 엄살쟁이
