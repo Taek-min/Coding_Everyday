f505f900dc30448bb81613698c218c1ffd1cf464
Deepred <deepred.kim@gmail.com>
2021-11-04 15:23:11 +0900

    Update ass_3058.java

--- Week11/Day73/ass_3058.java
+++ Week11/Day73/ass_3058.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 //// for과 ( 사이에 띄어쓰기를 하나 한다던지,
-//// 맥락이 바뀔때마다 줄바꿈을 하는 국룰(자바 코딩 컨벤션 가이드)이 있습니다. 깔끔한 코드를 위해 되도록 준수하는걸 권장해요
+//// 맥락이 바뀔때마다 줄바꿈을 한다던지 하는 국룰(자바 코딩 컨벤션 가이드)이 있습니다. 깔끔한 코드를 위해 되도록 준수하는걸 권장해요
 //// 국룰대로 바꾼 코드 예 => https://pastebin.com/AXYjLGEY
 public class Main{
 	public static void main(String[] args){
