c383d5cc11f792b48bc9348879b0e9e12ee88abf
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-11-04 13:09:56 +0900

    Create lye_2822.py

---
+++ Week11/Day74/lye_2822.py
@@ -0,0 +1,13 @@
+numList = [int(input()) for i in range(8)]
+top5 = []
+sumTop = 0
+
+for n in range(5):
+	maxN = numList.index(max(numList))
+	top5.append(maxN + 1)
+	sumTop += numList[maxN]
+	numList[maxN] = 0
+
+top5.sort()
+print(sumTop)
+print(' '.join(map(str,top5)))
