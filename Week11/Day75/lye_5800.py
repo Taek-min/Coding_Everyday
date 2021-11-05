c0e937daf36d9e6b3fcf48f4ec6fa06f9bee1cfd
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-11-05 17:37:52 +0900

    Create lye_5800.py

---
+++ Week11/Day75/lye_5800.py
@@ -0,0 +1,13 @@
+for i in range(int(input())):
+	scores = list(map(int, input().split()))
+	del scores[0]
+	scores.sort(reverse=True)
+	largestGap = 0
+	scoreCnt = len(scores)-1
+	
+	for j in range(0, scoreCnt):
+		gap = scores[j] - scores[j+1]
+		if abs(gap) > largestGap:
+			largestGap = gap
+	print("Class", i+1)
+	print('Max {0}, Min {1}, Largest gap {2}'.format(scores[0], scores[scoreCnt], largestGap))
