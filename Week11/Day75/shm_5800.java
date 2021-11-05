d6e4e5d0e4a4041a06a438e6e49e9c313fefc0bb
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2021-11-05 14:57:49 +0900

    Day75: problem solving

---
+++ Week11/Day75/shm_5800.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+		
+		for(int i = 0; i < testCase; i++) {
+			int amountOfSubject = sc.nextInt();
+			int[] scores = new int[amountOfSubject];
+			int max = 0;
+			for(int j = 0; j < scores.length; j++) {
+				scores[j] = sc.nextInt();
+			}
+			Arrays.sort(scores);
+			
+			for(int k = 1; k < scores.length; k++) {
+				if(max < scores[k] - scores[k - 1]) {
+					max = scores[k] - scores[k - 1];
+				}
+			}
+			System.out.printf("Class %d\n", i + 1);
+			System.out.printf("Max %d, Min %d, Largest gap %d\n", scores[amountOfSubject - 1], scores[0], max);
+		}		
+	}
+}
