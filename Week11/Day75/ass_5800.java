d2fe2dedfe611868b5d5cd373fb2234365b1b417
sungsuahn <saop1739@naver.com>
2021-11-07 04:02:59 +0900

    Update ass_5800.java

--- Week11/Day75/ass_5800.java
+++ Week11/Day75/ass_5800.java
@@ -5,21 +5,22 @@ public class Main{
 		Scanner sc = new Scanner(System.in);
 		int classX = sc.nextInt();
 		
-		for(int i = 1 ; i <= classX ; i++){
+		for (int i = 1; i <= classX; i++){
 			int students = sc.nextInt();
 			int point[] = new int[students];
 
-			for(int k = 0 ; k < students ; k++){
+			for (int k = 0; k < students; k++){
 				point[k] = sc.nextInt();
 			}
+			
 			Arrays.sort(point);
-            int gap = 0;
-      
-			for(int j = 1; j < point.length; j++) {
+			int gap = 0;
+			for (int j = 1; j < point.length; j++) {
 				if(gap < point[j] - point[j-1]) {
 					gap = point[j] - point[j-1];
 				}
 			}
+			
 			System.out.println("Class " + i);
 			System.out.println("Max " + point[students-1] + ", Min " + point[0] + ", Largest gap " + gap);
 		}
