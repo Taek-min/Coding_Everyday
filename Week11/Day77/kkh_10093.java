fc3ed4cd649712872ea1034c251e79028dba29da
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-11-07 23:44:09 +0900

    .

---
+++ Week11/Day77/kkh_10093.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		long num[] = Arrays.stream(in.nextLine().split(" ")).mapToLong(Long::parseLong).toArray();
+
+		Arrays.sort(num);
+		
+		if(num[0] == num[1]) {
+			System.out.println("0");
+		} else {
+			System.out.println(num[1] - num[0] - 1);
+		}
+		
+		for(long i = num[0] + 1; i < num[1]; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
