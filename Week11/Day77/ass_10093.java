3421a46278977d1cbf24593c2a35e9a2c2cb2474
sungsuahn <saop1739@naver.com>
2021-11-08 07:04:51 +0900

    Update ass_10093.java

--- Week11/Day77/ass_10093.java
+++ Week11/Day77/ass_10093.java
@@ -7,17 +7,17 @@ public class Main{
 		long b = sc.nextLong();
 		
 		if(a>b) {
-           System.out.println(a-b-1);
-           for(long i = b+1 ; i < a ; i++) {
+            System.out.println(a-b-1);
+            for(long i = b+1 ; i < a ; i++) {
                System.out.printf("%d ",i);
-           }
-       }else if(b>a) {
-           System.out.println(b-a-1);
-           for(long i = a+1 ; i< b ; i++) {
-               System.out.printf("%d ",i);
-           }
-       }else if(a==b) {
-		   System.out.println(0);
-	   }
+            }
+        }else if(b>a) {
+            System.out.println(b-a-1);
+            for(long i = a+1 ; i< b ; i++) {
+                System.out.printf("%d ",i);
+            }
+        }else if(a==b) {
+		    System.out.println(0);
+	    }
 	}
 }
