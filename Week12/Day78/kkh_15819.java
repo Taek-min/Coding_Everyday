7dc5d9be8ba093c0675fbb2d657d4cc4289d3267
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-11-08 22:24:14 +0900

    .

---
+++ Week12/Day78/kkh_15819.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int I = in.nextInt();
+		in.nextLine();
+		String[] str = new String[N];
+		
+		for(int i = 0; i < N; i++) {
+			str[i] = in.nextLine();
+		}
+		
+		Arrays.sort(str);
+		
+		System.out.println(str[I - 1]);
+	}
+}
