a93a988aaa0404fdf9f355f28836e51e654596b6
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-11-10 01:51:52 +0900

    Create lye_2775.py

---
+++ Week12/Day79/lye_2775.py
@@ -0,0 +1,14 @@
+caseCnt = int(input())
+
+for i in range(caseCnt):
+	floor = int(input())
+	roomNum = int(input())
+	
+	beforeRooms = [i for i in range(1, roomNum+1)]
+	
+	for j in range(floor):
+		floorSum = 0
+		for k in range(roomNum):
+			floorSum += beforeRooms[k]
+			beforeRooms[k] = floorSum
+	print(beforeRooms[roomNum-1])	
