16852e8d85caee07b5b018ead896ba3f7fe88ba8
Taek-min <jtm11245@gmail.com>
2021-11-10 18:05:09 +0900

    crystal

--- Week12/Day80/jtm_4158.java
+++ Week12/Day80/jtm_4158.java
@@ -4,24 +4,20 @@ import java.io.*;
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n;
-		int m;
-		int cnt;
+		int n , m, cnt;
 		HashSet<Integer> set;
-		
+
 		while(true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
+			set = new HashSet<>();
+			cnt = 0;
 			
 			if(n == 0 && m == 0) {
 				break;
 			}
-			
-			set = new HashSet<>();
-			cnt = 0;
-			
+		
 			for(int i = 0; i < n; i++) {
 				set.add(Integer.parseInt(br.readLine()));
 			}
