d2cb32a4b4672ebc46a79b987085557c315ebbcb
Taek-min <jtm11245@gmail.com>
2021-11-11 16:06:31 +0900

    ㅎㅏㄴㄱㅡㄹㅇㅣ ㅇㅏㄴㅊㅕㅈㅕ

---
+++ Week12/Day81/jtm_9655.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int stone = sc.nextInt();
+		
+		String result = (stone % 2 == 0) ? "CY" : "SK"; 
+		System.out.println(result);
+	}
+}
