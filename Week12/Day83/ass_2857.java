ea3424d45c76867bbf7af793a15f3b43c5dfab1a
sungsuahn <saop1739@naver.com>
2021-11-13 05:17:03 +0900

    d83

---
+++ Week12/Day83/ass_2857.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name[] = new String[5];
+        boolean check = true;
+
+        for (int i = 0; i < name.length; i++) {
+            name[i] = sc.next();
+        }
+
+        for (int i = 0; i < name.length; i++) {
+            if(name[i].contains("FBI")) {
+                System.out.print(i+1+" ");
+                check = false;
+            }
+        }
+
+        if(check) {
+            System.out.print("HE GOT AWAY!");
+        }
+    }
+}
--- Week12/README.md
+++ Week12/README.md
@@ -44,7 +44,7 @@
 
 | 문제                 | 답안 | 피드백 반영 답안 |
 | -------------------- | ---- | ---------------- |
-| [<sub>![Bronze2]</sub> FBI](https://www.acmicpc.net/problem/2857) | 창민 현민 택민 예은 가희 성수 | 창민 현민 택민 예은 가희 성수             |
+| [<sub>![Bronze2]</sub> FBI](https://www.acmicpc.net/problem/2857) | 창민 현민 택민 예은 가희 [성수](Day83/ass_2857.java)| 창민 현민 택민 예은 가희 성수             |
 
 ## [84일차](Day84)
 
