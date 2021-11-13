f426a3f0f015778bea94c93bb85084eafdab6cbc
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-11-13 20:51:39 +0900

    .

---
+++ Week12/Day81/kkh_fb_9655.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {//// 4번줄 지워주세요
+
+	public static void main(String[] args) { //// 따라하지마 
+		Scanner in = new Scanner(System.in);
+		
+		if(in.nextInt() % 2 == 1) {
+			System.out.println("SK");
+		} else {
+			System.out.println("CY");
+		}
+	}
+}
