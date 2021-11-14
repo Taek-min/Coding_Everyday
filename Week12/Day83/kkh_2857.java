53452aa6e64e80beb6c7239b0e64705b5f88e9e7
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-11-14 22:59:07 +0900

    .

---
+++ Week12/Day83/kkh_2857.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String result = "";
+		for(int i = 0; i < 5; i++) {
+			if(in.nextLine().contains("FBI")) {
+				result += (i + 1) + " ";
+			}
+		}
+		
+		if(result.equals("")) {
+			System.out.println("HE GOT AWAY!");
+		} else {
+			System.out.println(result);
+		}
+	}
+}
