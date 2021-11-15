c0ecebfa97e264340d9db18c0ec0e66e755a1704
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-11-15 14:36:40 +0900

    Create lye_5635.py

---
+++ Week13/lye_5635.py
@@ -0,0 +1,14 @@
+import datetime
+
+memCnt = int(input())
+nameList=[]
+dateList=[]
+
+for i in range(memCnt):
+	inputs = list(input().split())
+	nameList.append(inputs[0])
+	date = datetime.date(int(inputs[3]),int(inputs[2]),int(inputs[1]))
+	dateList.append(date)
+	
+print(nameList[dateList.index(max(dateList))])
+print(nameList[dateList.index(min(dateList))])
