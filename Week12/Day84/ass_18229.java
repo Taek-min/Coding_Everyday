01f162b4756afe0ce543f151af76ca558791a2bf
sungsuahn <saop1739@naver.com>
2021-11-16 04:05:32 +0900

    dass

---
+++ Week12/Day84/ass_18229.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int K = sc.nextInt();
+		int num[][] = new int[N][M];
+		int stretch[] = new int[N];
+
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < M; j++){
+				num[i][j] = sc.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < M; i++){
+			for (int j = 0; j < N; j++){
+				stretch[j] += num[j][i];
+				
+				if(K <= stretch[j]){
+					System.out.println((j + 1) + " " + (i + 1));
+					return;
+				}
+			}
+		}
+	}
+}
