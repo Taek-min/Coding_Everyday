95e76ce21e4074638afef49683a14b0696185972
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2021-11-16 23:23:26 +0900

    .

---
+++ Week13/Day86/kkh_11636.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int i = 0; i < T; i++) {
+			int turtles = 1;
+			int newTurtles = 0;
+			while(true) {
+				int nextTurtles = in.nextInt();
+				if(nextTurtles == 0) {
+					break;
+				}
+				
+				if((turtles * 2) < nextTurtles) {
+					newTurtles += nextTurtles - ((turtles * 2));
+				} 
+				
+				turtles = nextTurtles;
+			}
+			
+			System.out.println(newTurtles);
+		}
+	}
+}
