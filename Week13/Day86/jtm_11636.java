3e6129ee9fc812db0445cce73c0c1bb5f77500d3
Taek-min <jtm11245@gmail.com>
2021-11-17 18:49:37 +0900

    no message

--- Week13/Day86/jtm_11636.java
+++ Week13/Day86/jtm_11636.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int testCase = in.nextInt();
-		in.nextLine();
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+		sc.nextLine();
 
 		for(int i = 0; i < testCase; i++) {
 			int tmp = 0;
 			int incomes = 0;
-			int turtle[] =  Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			int turtle[] =  Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
 			for(int j = 0; j < turtle.length; j++) {
 				if(turtle[j]  > tmp * 2 && tmp != 0) {
