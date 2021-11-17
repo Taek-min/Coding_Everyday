29b12ed0604964ff63ac65bcc7ac650d37fc74df
Taek-min <jtm11245@gmail.com>
2021-11-17 22:16:56 +0900

    가히꺼

--- Week13/Day85/kkh_5635.java
+++ Week13/Day85/kkh_5635.java
@@ -1,1 +1,35 @@
-//// 착한 사람만 볼수 있습니다~
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {   
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      
+      int N = in.nextInt();
+      String minAgeName = "";
+      String MaxAgeName = "";
+      int min = 99999999;
+      int max = 0;  
+      SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+      Calendar date = Calendar.getInstance();
+      String today = format.format(date.getTime());
+      in.nextLine();
+      
+      for(int i = 0; i < N; i++) {
+         String student = in.nextLine();
+         String age = student.split(" ")[3] 
+                  + (student.split(" ")[2].length() == 1 ? "0" + student.split(" ")[2] : student.split(" ")[2]) 
+                  + (student.split(" ")[1].length() == 1 ? "0" + student.split(" ")[1] : student.split(" ")[1]);
+         if(Integer.parseInt(today) - Integer.parseInt(age) < min) {
+            minAgeName = student.split(" ")[0];
+            min = Integer.parseInt(today) - Integer.parseInt(age);
+         }
+         if(Integer.parseInt(today) - Integer.parseInt(age) > max) {
+            MaxAgeName = student.split(" ")[0];
+            max = Integer.parseInt(today) - Integer.parseInt(age);
+         }
+      }
+      
+      System.out.println(minAgeName + "\n" + MaxAgeName);
+   }
+}
