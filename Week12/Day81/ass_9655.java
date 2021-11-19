fca5f15efb171eace7d901e3e18aa3c66b4202a9
sungsuahn <saop1739@naver.com>
2021-11-19 13:05:05 +0900

    fb

--- Week12/Day81/ass_9655.java
+++ Week12/Day81/ass_9655.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int rook = sc.nextInt();  
-		if(rook%2==1){ //// rook % 2 == 1; 이런식으로 쓰면 더 가독성이 좋을거 같아요
+		if(rook % 2 == 1){ //// rook % 2 == 1; 이런식으로 쓰면 더 가독성이 좋을거 같아요
 			System.out.println("SK");
 		}else{
 			System.out.println("CY");
