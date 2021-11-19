02ae5b91b2e2cdb3bea01d43a35ea15c9fdbe6b1
sungsuahn <saop1739@naver.com>
2021-11-19 14:23:12 +0900

    Update ass_2204.java

--- Week13/Day89/ass_2204.java
+++ Week13/Day89/ass_2204.java
@@ -3,17 +3,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while(true) {
 			int word = Integer.parseInt(br.readLine());
 			if(word == 0) break;
-			String wordStr[] = new String[word];		
+			String wordStr[] = new String[word];
 			for (int i = 0; i < word; i++) {
 				wordStr[i] = br.readLine();
 			}
 			Arrays.sort(wordStr, String.CASE_INSENSITIVE_ORDER);
 			System.out.println(wordStr[0]);
 		}
-    }
+	}
 }
