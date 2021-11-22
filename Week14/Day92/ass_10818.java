85f7dc93d9f0d67618777f1767804c7db84eed9a
sungsuahn <saop1739@naver.com>
2021-11-23 02:11:51 +0900

    d92

---
+++ Week14/Day92/ass_10818.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+		int nums[] = new int [num];
+		
+		nums[0] = sc.nextInt();
+		int max = nums[0];
+		int min = nums[0];
+        
+		for (int i = 1; i < num; i++) {
+		    nums[i] = sc.nextInt();
+			if(max < nums[i]){
+				max = nums[i];
+			}
+			if(min > nums[i]){
+				min = nums[i];
+			}
+	    }
+		System.out.println(min + " " + max);
+    }
+}
