8c4da0f8fb3fe3529e19590b860d219aa3ef5ea7
ckdals6932 <ckdals6932@naver.com>
2022-02-03 15:16:12 +0900

    96일차 문제풀이

---
+++ Week15/Day96/kcm_17356.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double wook = in.nextInt();
+		double jee = in.nextInt();
+		double Wookjee = (jee - wook) / 400;
+		double win =  1 / (1 + Math.pow(10, Wookjee));
+		System.out.println(win);
+	}
+}
