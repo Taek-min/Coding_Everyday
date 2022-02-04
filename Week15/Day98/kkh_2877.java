0e703523c4fce0737187599efdd3ba828e2eedfe
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2022-02-05 02:58:47 +0900

    .

---
+++ Week15/Day98/kkh_2877.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);		
+		int K = in.nextInt();		
+		String K2 = Integer.toBinaryString(K + 1);
+		
+		for(int i = 1; i < K2.length(); i++) {
+			if(K2.substring(i, i + 1).equals("0")) {
+				System.out.print("4");
+			} else {
+				System.out.print("7");
+			}
+		}
+	}
+}
