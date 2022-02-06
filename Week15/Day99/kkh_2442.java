ffde691fd496d4b7ba42b26902e629e399beb915
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2022-02-07 00:48:14 +0900

    .

---
+++ Week15/Day99/kkh_2442.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < 2 * N - 1; j++) {
+				if ((j < N && N - i - 1 <= j) || (j + 1 >= N && j - N < i)) {
+					System.out.print("*");
+				} else if (N - i - 1 > j) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
