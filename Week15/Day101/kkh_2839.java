dce22d1c073f6dc0639bcfe668bd25b53b148402
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2022-02-08 22:17:00 +0900

    .

---
+++ Week15/Day101/kkh_2839.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int cnt = 0;
+		
+		while(N > 2) {
+			if(N % 5 == 0) {
+				cnt += N / 5;
+				N = 0;
+			} else {
+				cnt++;
+				N -= 3;
+			}
+		}
+			
+		if(N != 0) {
+			System.out.println("-1");
+		} else {
+			System.out.println(cnt);
+		}
+	}
+}
