fafa94fc9e92df8f511559870803cb8234cfbf0d
Taek-min <79956027+Taek-min@users.noreply.github.com>
2022-02-09 17:18:12 +0900

    Create jtm_2839.java

---
+++ Week15/Day101/jtm_2839.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int bag = 0;
+		
+		while(true){
+		    if(N%5 == 0){
+		        bag += N / 5;
+		        System.out.println(bag);
+		        break;
+		    }
+		    else{
+		        N -= 3;
+		        bag++;
+		    }
+		    
+		    if(N < 0){
+		        System.out.println(-1);
+		        break;
+        }
+    }
+	}
+}
