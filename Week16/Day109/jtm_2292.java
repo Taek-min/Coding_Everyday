35d01144f0810091cb46b7d8ca8a12f40eff156d
PC097\Administrator <jtm11245@gmail.com>
2022-02-21 15:12:37 +0900

    no message

---
+++ Week16/Day109/jtm_2292.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		int count = 1;
+		int range = 1;	
+
+		while(true) {
+			if(range >= num) {
+				break;
+			}
+			range += 6 * count;
+			count++;
+		}
+
+		System.out.println(count);
+	}
+}
+// 멋져 
