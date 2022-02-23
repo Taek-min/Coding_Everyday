caff896719eb500ef26c31fb6bd5757a68dcaad7
ckdals6932 <ckdals6932@naver.com>
2022-02-23 16:10:31 +0900

    문제 풀이 및 피드백

--- Week17/Day112/jtm_4740.java
+++ Week17/Day112/jtm_4740.java
@@ -12,3 +12,5 @@ public class Main {
 			System.out.println(sb.reverse());		
 		}
 	}
+	//  다 똒같네 ?
+	
---
+++ Week17/Day113/kcm_10250.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class kcm_10250 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCase = in.nextInt();
+		
+		for(int i = 0; i < testCase; i++) {
+			int h = in.nextInt();
+			int w = in.nextInt();
+			int n = in.nextInt();
+			if( n % h > 0) {
+				System.out.println(n % h * 100 + n / h + 1);
+			}
+			else {
+				System.out.println(h * 100 + n / h);
+			}
+		}
+	}
+}
